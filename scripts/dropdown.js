5d2b7474ae4eeba0e5dcf1886578303f45620fce
Jack Hermanson <jack.hermanson@live.com>
2021-10-28 16:34:34 -0600

    fix the dropdown toggler

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -2,25 +2,22 @@ const dropdownToggles = Array.from(
     document.querySelectorAll(".dropdown-toggle")
 );
 
-const showElement = (targetElement, toggleElement) => {
-    targetElement.style.display = "block";
-    toggleElement.addEventListener("click", () => hideElement(targetElement, toggleElement));
-    toggleElement.classList.remove("fa-caret-down");
-    toggleElement.classList.add("fa-caret-up");
-}
-
-const hideElement = (targetElement, toggleElement) => {
-    targetElement.style.display = "none";
-    toggleElement.addEventListener("click", () => showElement(targetElement, toggleElement));
-    toggleElement.classList.remove("fa-caret-up");
-    toggleElement.classList.add("fa-caret-down");
-}
-
 for (let toggleElement of dropdownToggles) {
     const targetElementSelector = toggleElement.getAttribute("data-target");
     const targetElement = document.querySelector(targetElementSelector);
+    targetElement.style.display = "none";
 
-    toggleElement.addEventListener("click", () => showElement(targetElement, toggleElement));
+    toggleElement.addEventListener("click", () => {
+        if (targetElement.style.display === "none") {
+            targetElement.style.display = "block";
+            toggleElement.classList.remove("fa-caret-down");
+            toggleElement.classList.add("fa-caret-up");
+        } else {
+            targetElement.style.display = "none";
+            toggleElement.classList.remove("fa-caret-up");
+            toggleElement.classList.add("fa-caret-down");
+        }
+    });
 }
 
 
